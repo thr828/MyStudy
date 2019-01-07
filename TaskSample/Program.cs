3ba0d7a4aec53e835f4a7e99a06d69faf3c745f3
thr828 <592040808@qq.com>
2019-01-07 20:24:23 +0800

    添加task三种声明方式

---
+++ TaskSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskSample
+{
+    /// <summary>
+    /// 声明task的三种方法
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.using taskfactory
+            TaskFactory task = new TaskFactory();
+            var t1 = task.StartNew(cal);
+
+            //2.using Task.Factory
+            var t2 = Task.Factory.StartNew(cal);
+            //3.task构造函数
+            Task t3 = new Task(cal);
+            t3.Start();
+
+
+            Task t4 = new Task(cal, TaskCreationOptions.PreferFairness);
+            t4.Start();
+
+        }
+
+        public static void cal()
+        {
+            Console.WriteLine($"current thread id:{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
