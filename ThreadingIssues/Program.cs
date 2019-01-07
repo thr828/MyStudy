2c14de92eb267303690724dbb2e388ac2af124c0
thr828 <592040808@qq.com>
2019-01-07 23:34:56 +0800

    线程安全 加锁

--- ThreadingIssues/Program.cs
+++ ThreadingIssues/Program.cs
@@ -13,7 +13,8 @@ namespace ThreadingIssues
     /// 在给包含 5的变量递增了 1后 ,可能希望该变量的值就是 6。 
     /// 但事实不一定是这样。例如,如果一个线程刚刚执行完  if (state == 5)语句,它就被其他线程抢占,调度器运行另一个线程。第二个线程现在进入if体,因为 state的值仍是 5,所以将它递增到 6。 
     /// 第一个线程现在再次被调度,在下一条语旬中, state递增到 7。 这时就发生了争用条件,并显示断言消息。
-    /// 解决方法：要避免该问题,可 以锁定共享的对象。这可以在线程中完成:用下面的lock语 句锁定在线程中共享的 state变量。只有一个线程能在锁定块中处理共享的 state对象。由于这个对象在所有的线程之间共享, 因此如果一个线程锁定了state, 另 一个线程就必须等待该锁定的解除。一旦接受锁定,线程就拥有该锁定,直到该锁定块的末尾才解除锁定。如果改变 state变量引用的对象的每个线程都使用一个锁定,就不会出现争用条件
+    /// 解决方法1：要避免该问题,可 以锁定共享的对象。这可以在线程中完成:用下面的lock语 句锁定在线程中共享的 state变量。只有一个线程能在锁定块中处理共享的 state对象。由于这个对象在所有的线程之间共享, 因此如果一个线程锁定了state, 另 一个线程就必须等待该锁定的解除。一旦接受锁定,线程就拥有该锁定,直到该锁定块的末尾才解除锁定。如果改变 state变量引用的对象的每个线程都使用一个锁定,就不会出现争用条件
+    /// 解决方法2：在使用共享对象时,除 了进行锁定之外,还可以将共享对象设置为线程安全的对象。其中ChangeState方法包含一条lock语句。由于不能锁定 state变 量本身(只有引用类型才能用于锁勋 ,因此定义一个 object类型的变量 sync 将它用于 lock语句。如果每次 state 的值更改时, 都使用同一个同步对象来锁定, 就不会出现争用条件。
     /// </summary>
     class Program
     {
@@ -47,15 +48,19 @@ namespace ThreadingIssues
     public class StateObject
     {
         private int state = 5;
+        private object sync = new object();
         public void ChangeState(int loop)
         {
-
-            if (state == 5)
+            lock (sync)
             {
-                state++;
-                Trace.Assert(state == 6, "Race condition occurred after " + loop + " loops");
+                if (state == 5)
+                {
+                    state++;
+                    Trace.Assert(state == 6, "Race condition occurred after " + loop + " loops");
+                }
+                state = 5;
             }
-            state = 5;
+            
 
         }
     }
@@ -71,10 +76,10 @@ namespace ThreadingIssues
             int i = 0;
             while (true)
             {
-                lock (state)//加锁 因为是同一个StateObject对象  共享对象
-                {
+                //lock (state)//加锁1 因为是同一个StateObject对象  共享对象
+                //{
                     state.ChangeState(i++);
-                }
+                //}
                 
             }
 
